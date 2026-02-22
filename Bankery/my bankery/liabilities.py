244275644093be6998aec2ec8d0996c6cb019348
Florence Cheung <ftcheung@unc.edu>
2026-02-22 03:09:22 -0500

    Delete Bankery/my bankery directory

--- Bankery/my bankery/liabilities.py
+++
@@ -1,9 +0,0 @@
-class Liabilities:
-    loan: float
-    acc_payable: float
-    total: float
-
-    def __init__(self):
-        self.loan = 1
-        self.acc_payable = 1
-        self.total = self.loan + self.acc_payable
